171914a27e9c4b2eb1610d8206dc9e967f965c7f
An <140888823+ancJ29@users.noreply.github.com>
2023-11-07 17:08:03 +0700

    sync@20231107.1708

--- src/components/Login/Form/index.tsx
+++ src/components/Login/Form/index.tsx
@@ -1,21 +1,14 @@
 import PasswordInput from "@/components/common/PasswordInput";
+import TextCenter from "@/components/common/TextCenter";
 import TextInput from "@/components/common/TextInput";
 import useTranslation from "@/hooks/useTranslation";
 import callApi from "@/services/api";
 import useAuthStore from "@/stores/auth.store";
-import { Anchor, Button, Card, Center, Checkbox, Flex, Group, Stack, Text } from "@mantine/core";
-import { useForm, zodResolver } from "@mantine/form";
-import { IconLock } from "@tabler/icons-react";
+import { Anchor, Button, Card, Checkbox, Flex, Group, Stack } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { z } from "zod";
-
-const schema = z.object({
-  password: z.string().min(1, { message: "Please enter password" }),
-  name: z.string().min(1, { message: "Please enter Email" }),
-});
-const resolver = zodResolver(schema);
-type LoginProps = z.infer<typeof schema>;
 
 const LoginForm = () => {
   const t = useTranslation();
@@ -25,7 +18,10 @@ const LoginForm = () => {
       name: "",
       password: "",
     },
-    validate: resolver,
+    validate: {
+      name: (value) => (value.length < 1 ? t("Please enter Email") : null),
+      password: (value) => (value.length < 1 ? t("Please enter Password") : null),
+    },
   });
   const navigate = useNavigate();
   const { setToken } = useAuthStore();
@@ -41,7 +37,6 @@ const LoginForm = () => {
         navigate("/dashboard");
       } else {
         form.setErrors({
-          name: t("Email or password is incorrect."),
           password: t("Email or password is incorrect."),
         });
       }
@@ -50,7 +45,7 @@ const LoginForm = () => {
   );
   return (
     <Card withBorder shadow='md' radius={10} mt='1rem'>
-      <Stack gap='xs' p='.5rem'>
+      <Stack gap='xs' p='.5rem' pt={0}>
         <form onSubmit={form.onSubmit(onLogin)}>
           <TextInput
             withAsterisk
@@ -67,12 +62,9 @@ const LoginForm = () => {
           />
           <Group justify='flex-start' mt='xl'>
             <Flex w='100%' fz='0.8rem' justify='space-between'>
-              <Checkbox defaultChecked label='Remember me' />
+              <Checkbox defaultChecked label={t("Remember me")} />
               <Anchor href='/forgot-password' underline='never'>
-                <Center>
-                  <IconLock size='1rem' />
-                  <Text>{t("Forgot your password")}?</Text>
-                </Center>
+                <TextCenter>{t("Forgot your password")}?</TextCenter>
               </Anchor>
             </Flex>
             <Button type='submit' w='100%'>
@@ -86,3 +78,9 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
+
+const schema = z.object({
+  password: z.string(),
+  name: z.string(),
+});
+type LoginProps = z.infer<typeof schema>;
