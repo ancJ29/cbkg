6d845b407e791992b80c1f3e85621a8fc6cebf44
An <140888823+ancJ29@users.noreply.github.com>
2023-12-14 00:51:10 +0700

    chore: fix bug

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,7 +1,6 @@
 export * from "./branch";
 export * from "./chain";
 export * from "./common/api";
-export * from "./common/status";
 export * from "./option";
 export * from "./store";
 export * from "./user";
